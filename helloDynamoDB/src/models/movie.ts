5334fb57a7ff0321c481eaf24ea6e63bf259f7d0
Kennytian <kt.tian@gmail.com>
2019-03-16 12:35:17 +0800

    Refactor code

---
+++ helloDynamoDB/src/models/movie.ts
@@ -0,0 +1,15 @@
+export const modelsMovie = {
+  AttributeDefinitions: [ // 主键数据类型
+    { AttributeName: "year", AttributeType: "N" }, // N Number
+    { AttributeName: "title", AttributeType: "S" },   // S String
+  ],
+  KeySchema: [       // 主键
+    { AttributeName: "year", KeyType: "HASH" },  // Partition key 分区键
+    { AttributeName: "title", KeyType: "RANGE" },  // Sort key 排序键
+  ],
+  ProvisionedThroughput: { // DynamoDB 吞吐量配置
+    ReadCapacityUnits: 20,
+    WriteCapacityUnits: 20,
+  },
+  TableName: "Movies", // 表名
+};
--- helloDynamoDB/src/table.ts
+++ helloDynamoDB/src/table.ts
@@ -1,26 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import getDynamoClient from "./dynamo-client";
+import { modelsMovie } from "./models/movie";
 
 export const create: APIGatewayProxyHandler = async (event) => {
   const dynamodb = getDynamoClient(event);
-  const params = {
-    AttributeDefinitions: [ // 主键数据类型
-      { AttributeName: "year", AttributeType: "N" }, // N Number
-      { AttributeName: "title", AttributeType: "S" },   // S String
-    ],
-    KeySchema: [       // 主键
-      { AttributeName: "year", KeyType: "HASH" },  // Partition key 分区键
-      { AttributeName: "title", KeyType: "RANGE" },  // Sort key 排序键
-    ],
-    ProvisionedThroughput: { // DynamoDB 吞吐量配置
-      ReadCapacityUnits: 20,
-      WriteCapacityUnits: 20,
-    },
-    TableName: "Movies", // 表名
-  };
   try {
-    const { TableDescription = {} } = await dynamodb.createTable(params).promise();
+    const { TableDescription = {} } = await dynamodb.createTable(modelsMovie).promise();
     return {
       body: JSON.stringify({
         creationDateTime: TableDescription.CreationDateTime,
