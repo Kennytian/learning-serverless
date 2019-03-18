c35aea94910add49410233a33637c0faa5773816
Kennytian <kt.tian@gmail.com>
2019-03-18 08:40:47 +0800

    Add save and update features

--- helloDynamoDB/src/table.ts
+++ helloDynamoDB/src/table.ts
@@ -66,11 +66,59 @@ export const importData: APIGatewayProxyHandler = async event => {
   }
 };
 
+export const saveData: APIGatewayProxyHandler = async event => {
+  const params = {
+    TableName: 'Movies',
+    Item: {
+      year: 2015,
+      title: 'The Big New Movie',
+      info: {
+        plot: 'Nothing happens at all.',
+        rating: 0,
+      },
+    },
+  };
+  try {
+    const dynamodbDoc = dynamoDBDocumentClient(event);
+    const result = await dynamodbDoc.put(params).promise();
+    return {
+      body: JSON.stringify({ ...result, message: `Table save data successfully!` }),
+      statusCode: 200,
+    };
+  } catch (e) {
+    return getExceptionBody(e);
+  }
+};
+
 export const getData: APIGatewayProxyHandler = async event => {
   const dynamodbDoc = dynamoDBDocumentClient(event);
   const params = { TableName: 'Movies', Key: { year: 2013, title: 'Rush' } };
   try {
     const result = await dynamodbDoc.get(params).promise();
+    return {
+      body: JSON.stringify(result),
+      statusCode: 200,
+    };
+  } catch (e) {
+    return getExceptionBody(e);
+  }
+};
+
+export const updateData: APIGatewayProxyHandler = async event => {
+  const dynamodbDoc = dynamoDBDocumentClient(event);
+  const params = {
+    TableName: 'Movies',
+    Key: { year: 2015, title: 'The Big New Movie' },
+    UpdateExpression: 'set info.rating=:r, info.plot=:p, info.actors=:a',
+    ExpressionAttributeValues: {
+      ':r': 5.5,
+      ':p': 'Everything happens all at once.',
+      ':a': ['Larry', 'Moe', 'Curly']
+    },
+    ReturnValues: 'UPDATE_NEW',
+  };
+  try {
+    const result = await dynamodbDoc.update(params).promise();
     return {
       body: JSON.stringify(result),
       statusCode: 200,
